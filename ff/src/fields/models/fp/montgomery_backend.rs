1566736efce471e8abfdfae085b91d2a7feea4c8
developeruche <developeruche@gmail.com>
2025-08-11 11:56:09 +0100

    patch-4

--- ff/src/fields/models/fp/montgomery_backend.rs
+++ ff/src/fields/models/fp/montgomery_backend.rs
@@ -806,12 +806,12 @@ impl<T: MontConfig<N>, const N: usize> Fp<MontBackend<T, N>, N> {
         (carry2 != 0, self)
     }
 
-    const fn mul(self, other: &Self) -> Self {
-        // #[cfg(all(
-        //     target_os = "zkvm",
-        //     target_vendor = "succinct",
-        //     target_arch = "riscv32"
-        // ))]
+    fn mul(self, other: &Self) -> Self {
+        #[cfg(all(
+            target_os = "zkvm",
+            target_vendor = "succinct",
+            target_arch = "riscv32"
+        ))]
         {
             let a = Fp::<MontBackend<T, N>, N>::new(succinct::modmul_uint_256(&self.0, &other.0, &Self::MODULUS));
             return a;
